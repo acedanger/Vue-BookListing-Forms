b203b16fc5d44316a387c301b2db43a36298d14f
Peter Wood <acedanger49@gmail.com>
2018-12-01 13:06:50 -0500

    fixed typo in bookData object

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -35,7 +35,7 @@ export default {
       this.books.push({
         title: bookData.bookTitle
         , author: bookData.bookAuthor
-        , finishedReading: bookdata.finishedReading
+        , finishedReading: bookData.finishedReading
       });
     }
   }
